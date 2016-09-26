e7471c37a533b2db45e50329824ddf561b31440e
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-09-26 12:08:56 +0200

    Added node as platform for debug purpose

--- platforms/ios/App/App/AppDelegate.swift
+++ platforms/ios/App/App/AppDelegate.swift
@@ -23,6 +23,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = HomeViewController()
         
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
