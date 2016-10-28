d1eb5076f602d836a1f5c4effec1fe5ee061f3b6
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-10-29 00:30:14 +0200

    Building web platform

--- platforms/web/assets/js/ajweb.js
+++ platforms/web/assets/js/ajweb.js
@@ -1,6 +1,159 @@
 "use strict";
 
 (function(exports) {
+    /**
+     *
+     *  Base64 encode / decode
+     *  http://www.webtoolkit.info/
+     *
+     **/
+    var Base64 = {
+        // private property
+        _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+        // public method for encoding
+        encode : function (input) {
+            var output = "";
+            var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = Base64._utf8_encode(input);
+
+            while (i < input.length) {
+
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                    this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+            }
+
+            return output;
+        },
+
+        // public method for decoding
+        decode : function (input) {
+            var output = "";
+            var chr1, chr2, chr3;
+            var enc1, enc2, enc3, enc4;
+            var i = 0;
+
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            while (i < input.length) {
+
+                enc1 = this._keyStr.indexOf(input.charAt(i++));
+                enc2 = this._keyStr.indexOf(input.charAt(i++));
+                enc3 = this._keyStr.indexOf(input.charAt(i++));
+                enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+            }
+
+            output = Base64._utf8_decode(output);
+
+            return output;
+
+        },
+
+        // private method for UTF-8 encoding
+        _utf8_encode : function (string) {
+            string = string.replace(/\r\n/g,"\n");
+            var utftext = "";
+
+            for (var n = 0; n < string.length; n++) {
+
+                var c = string.charCodeAt(n);
+
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                }
+                else if((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+                else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                }
+
+            }
+
+            return utftext;
+        },
+
+        // private method for UTF-8 decoding
+        _utf8_decode : function (utftext) {
+            var string = "";
+            var i = 0;
+            var c = 0, c1 = 0, c2 = 0, c3 = 0;
+
+            while ( i < utftext.length ) {
+
+                c = utftext.charCodeAt(i);
+
+                if (c < 128) {
+                    string += String.fromCharCode(c);
+                    i++;
+                }
+                else if((c > 191) && (c < 224)) {
+                    c2 = utftext.charCodeAt(i+1);
+                    string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                    i += 2;
+                }
+                else {
+                    c2 = utftext.charCodeAt(i+1);
+                    c3 = utftext.charCodeAt(i+2);
+                    string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                    i += 3;
+                }
+
+            }
+
+            return string;
+        }
+
+    };
+
+    /**
+     *
+     * async
+     */
+
+    var async = exports.async = function(action) {
+        setTimeout(action, 0);
+    };
+
+    /**
+     * path utils
+     */
 
     var path = {
         separator: "/",
@@ -190,6 +343,39 @@
     })();
 
     /**
+     * Buffers
+     */
+
+    var Buffer = exports.Buffer = {
+        buffers: [],
+        lastBufferId: 0,
+
+        create: function(bytes) {
+            var buffer = {
+                id: this.lastBufferId++,
+                data: bytes
+            };
+            this.buffers.push(buffer);
+            return buffer.id;
+        },
+
+        get: function(id) {
+            var bytes = _.find(this.buffers, function(b) {return b.id == id});
+            if (bytes != null) {
+                this.destroy(buffer.id);
+            }
+
+            return buffer.data;
+        },
+
+        destroy: function(id) {
+            this.buffers = _.find(this.buffers, function(b) {return b.id != id});
+        }
+
+
+    };
+
+    /**
      * Logger
      */
 
@@ -216,9 +402,95 @@
             } else {
                 console.warn("AJ: " + msg);
             }
-        },
-    }
+        }
+    };
 
+    /**
+     * Http
+     */
+
+    exports.__httpClient = {
+        request: function(url, method, data, headers, accept, contentType, rawResponse, cb) {
+            $.ajax({
+                url: url,
+                method: method,
+                data: data,
+                accept: accept,
+                contentType: contentType,
+                success: function(response) {
+                    cb(false, response);
+                },
+                error: function(xhr, err) {
+                    cb(true, err);
+                }
+            })
+        }
+    };
+
+    /**
+     * Assets
+     */
+
+    exports.__assetsManager = {
+        load: function(path, cb) {
+            $.ajax({
+                url: url,
+                method: "GET",
+                success: function(response) {
+                    cb(false, response);
+                },
+                error: function(xhr, err) {
+                    cb(true, err);
+                }
+            })
+        },
+
+        exists: function(path, cb) {
+            cb(false, true);
+        }
+    };
+
+
+    /**
+     * Storage
+     */
+
+    exports.__storageManager = {
+        readText: function(path, cb) {},
+        read: function(path, cb) {},
+        writeText: function(path, content, cb) {},
+        write: function(path, buffer, cb) {},
+        delete: function(path, cb) {},
+        exists: function(path, cb) {}
+    };
+
+    /***
+     * Buffers manager
+     */
+
+    exports.__buffersManager = {
+        create: function(base64, cb) {
+            async(function() {
+                var bytes = Base64.encode(base64);
+                cb(false, Buffer.create(bytes));
+            });
+
+        },
+
+        get: function(id, cb) {
+            async(function() {
+                var base64 = Base64.decode(Buffer.get(id));
+                cb(false, base64);
+            });
+        },
+
+        destroy: function(id, cb) {
+            async(function() {
+                Buffer.destroy(id);
+                cb(false, "");
+            });
+        }
+    };
 
     /**
      * AJ Web Runtime
