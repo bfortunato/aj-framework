96fe84e5a84a052d2d808df784bca84a24a1d4ed
Bruno Fortunato <bruno.fortunato@applica.guru>
2016-11-24 18:20:40 +0100

    Bug fixing

--- platforms/ios/Libs/AJ/AJ/AJTimers.swift
+++ platforms/ios/Libs/AJ/AJ/AJTimers.swift
@@ -25,24 +25,25 @@ class AJTimerAction {
     let action: JSValue
     let delayTime: Int
     var canceled: Bool = false
+    var destroyed: Bool = false
     var loop = false
-    
+
     weak var timers: AJTimers?
-    
+
     init(action: JSValue, delay: Int) {
         AJTimerActionCounter += 1
         id = AJTimerActionCounter
         self.action = action
         self.delayTime = delay
     }
-    
+
     func execute() {
         async {
             while true {
                 Thread.sleep(forTimeInterval: Double(self.delayTime) / 1000.0)
                 if (!self.canceled) {
                     self.action.call(withArguments: [])
-                    
+
                     if (!self.loop) {
                         break
                     }
@@ -50,59 +51,63 @@ class AJTimerAction {
                     break
                 }
             }
-            
-            self.timers?.destroy(self)
+
         }
     }
-    
+
     func cancel() {
         canceled = true
+    }
+
+    func destroy() {
+        destroyed = true
     }
 }
 
 @objc
 class AJTimers: NSObject, AJTimersProtocol {
-    
+
     var actions = [AJTimerAction]()
-    
+
     func setTimeout(_ action: JSValue, _ delay: Int) -> Int {
         let timerAction = AJTimerAction(action: action, delay: delay)
         timerAction.loop = false
         timerAction.timers = self
-        
-        actions.append(timerAction)
-        
+
+        append(action: timerAction)
+
         timerAction.execute()
-        
+
         return timerAction.id
     }
-    
+
     func setInterval(_ action: JSValue, _ delay: Int) -> Int {
         let timerAction = AJTimerAction(action: action, delay: delay)
         timerAction.loop = true
         timerAction.timers = self
-        
+
         actions.append(timerAction)
-        
+
         timerAction.execute()
-        
+
         return timerAction.id
     }
-    
+
+    func append(action: AJTimerAction) {
+        //first of all, clear destroyed times
+        actions = actions.filter({$0.id == action.id})
+        actions.append(action)
+
+    }
+
     func clearTimeout(_ timerId: Int) {
         if let timerAction = actions.filter({ $0.id == timerId }).first {
             timerAction.cancel()
         }
     }
-    
+
     func clearInterval(_ timerId: Int) {
         clearTimeout(timerId)
-    }
-    
-    func destroy(_ timerAction: AJTimerAction) {
-        if let index = actions.index(where: {$0.id == timerAction.id}) {
-            actions.remove(at: index)
-        }
     }
 
 }
